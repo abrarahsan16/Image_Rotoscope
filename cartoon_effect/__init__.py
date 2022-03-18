a55f322a8281d12052f828db8fd0a7ec2adb6370
Abrar Ahsan <ahsan.abrar16@hotmail.com>
2022-03-18 09:37:53 -0400

    Initialize

---
+++ cartoon_effect/__init__.py
@@ -0,0 +1,6 @@
+# NOTE: Don't implement the 'CartoonEffect' class in this file.  Instead, create
+# another file and then import it into this file.  Python's module importer will
+# then make it look like the other module doesn't exist.
+#
+# from cartoon_effect.<insert-module-name> import CartoonImage
+#
