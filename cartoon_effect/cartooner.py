d6345d6eab7f1bcc1fb797ab39969d923a53a185
Abrar Ahsan <ahsan.abrar16@hotmail.com>
2022-04-05 22:02:40 -0400

    Adjusting stylization

--- cartoon_effect/cartooner.py
+++ cartoon_effect/cartooner.py
@@ -4,129 +4,164 @@ import math
 from skimage.color import rgb2grey
 from skimage.morphology import label
 
-class CartoonEffect:
-
-    def stylization(self, img, imgGrey, sigmaBlur, row, T, N):
-        imgNew = imgGrey
-        # Apply gaussian blur on the image
-        imgBlur = self.gaussianBlur(imgNew, sigmaBlur)
-        # Find the max and minimum intensities of the image
-        Imin = np.min(imgBlur)
-        Imax = np.max(imgBlur)
-        # Set the total number of bins
-        bins = np.clip((Imax-Imin)/N*np.arange(N+1) + Imin, 0, 255)
-        # Allocate image L with same size as imgBlur
-        L = np.zeros(img.shape[:2], dtype=int)
-        # fill in L
-        for i in range(N):
-            if i == N-1:
-                mask = np.logical_and(imgBlur >= bins[i], imgBlur < bins[i+1])
-            else:
-                mask = imgBlur >= bins[i]
-            L[mask] = i
-        # Allocate image S with same size as imgBlur
-        S = np.zeros(img.shape)
-        # If original image is RGB or Grey
-        C = np.zeros(img.shape, dtype = int)
-        if img.ndim == 3:
-            L = label(L)
-            for i in range(255):
-                Li = L == i
-                Li_Connected = np.stack((Li, Li, Li), axis = 2)
-                mean_Colour = np.mean(imgBlur, where = Li_Connected, axis=(0, 1))
-                C[Li] = mean_Colour
+def stylization(img, imgGrey, sigmaBlur, sigmaDog, p, N):
+    imgNew = imgGrey
+    # Apply gaussian blur on the image
+    imgBlur = gaussianBlur(imgNew, sigmaBlur)
+    # Find the max and minimum intensities of the image
+    Imin = np.min(imgBlur)
+    Imax = np.max(imgBlur)
+    # Set the total number of bins
+    bins = np.clip((Imax-Imin)/N*np.arange(N+1) + Imin, 0, 255)
+    # Allocate image L with same size as imgBlur
+    L = np.zeros(imgBlur.shape, dtype=int)
+    # fill in L
+    for i in range(N):
+        if i == N-1:
+            mask = np.logical_and(imgBlur >= bins[i], imgBlur < bins[i+1])
         else:
-            C = C/N
+            mask = imgBlur >= bins[i]
+        L[mask] = i
+    # Allocate image S with same size as imgBlur
+    S = np.zeros(img.shape)
+    C = np.zeros(img.shape, dtype = int)
+    # If original image is RGB or Grey
+    if img.ndim == 3:
+        L = label(L)
+        for i in range(255):
+            Li = L == i
+            Li_Connected = np.stack((Li, Li, Li), axis = 2)
+            mean_Colour = np.mean(img, where = Li_Connected)
+            C[Li] = mean_Colour
+        XDog = np.ones((img.shape[0], img.shape[1], img.shape[2]))
+        XDog[:, :, 0] = XDoG(img, sigmaDog, p)
+        XDog[:, :, 1] = XDog[:, :, 0]
+        XDog[:, :, 2] = XDog[:, :, 0]
+        return C * XDog
+    else:
+        C = C/N
         # Return output
-        return C * self.XDog(img, sigmaBlur, row, T)
+        return C * XDoG(img, sigmaDog, p)
 
-    def gaussianBlur(img, imgGrey, sigma):
-        '''Filter an image using a Gaussian kernel.
-        The Gaussian is implemented internally as a two-pass, separable kernel.
+def gaussianBlur(img, sigma):
+    '''Filter an image using a Gaussian kernel.
+    The Gaussian is implemented internally as a two-pass, separable kernel.
 
-        Note
-        ----
-        The kernel is scaled to ensure that its values all sum up to '1'.  The
-        slight truncation means that the filter values may not actually sum up to
-        one.  The normalization ensures that this is consistent with the other
-        low-pass filters in this assignment.
+    Note
+    ----
+    The kernel is scaled to ensure that its values all sum up to '1'.  The
+    slight truncation means that the filter values may not actually sum up to
+    one.  The normalization ensures that this is consistent with the other
+    low-pass filters in this assignment.
 
-        Parameters
-        ----------
-        img : numpy.ndarray
-            a greyscale image
-        sigma : float
-            the width of the Gaussian kernel; must be a positive, non-zero value
+    Parameters
+    ----------
+    img : numpy.ndarray
+        a greyscale image
+    sigma : float
+        the width of the Gaussian kernel; must be a positive, non-zero value
 
-        Returns
-        -------
-        numpy.ndarray
-            the Gaussian blurred image; the output will have the same type as the
-            input
+    Returns
+    -------
+    numpy.ndarray
+        the Gaussian blurred image; the output will have the same type as the
+        input
 
-        Raises
-        ------
-        ValueError
-            if the value of sigma is negative
-        '''
-        if sigma <= 0.01:
-            raise ValueError('Sigma should be positive.')
-        N = max(6*sigma, 3)
-        # If the dimension is even, add 1 to make it odd
-        if N % 2 == 0:
-            N = N + 1
-        # Create one dimension kernel of ascending values
-        x_kernel = np.arange(0, N, step=1, dtype=np.float32)
-        # Apply the gaussian blur equation
-        x_kernel = 1/np.sqrt(2*np.pi*sigma**2) * np.exp((-(x_kernel-math.floor(0.5*N))**2)/(2*sigma**2))
-        # Ensure that the shape of the array is correct
-        x_kernel = x_kernel.reshape((int(len(x_kernel)), 1))
-        # Transpose to create the other dimension
-        y_kernel = np.transpose(x_kernel)
-        y_kernel = y_kernel.reshape((1, int(len(x_kernel))))
-        # Find the sum of the kernel values
-        kernel_sum = (x_kernel*y_kernel).sum()
-        # Do convolutions one dimension at a time
-        conv = self.convolve(img, y_kernel)
-        conv = self.convolve(conv, x_kernel)
-        # Return the normalized output
-        return conv/(kernel_sum)
-        raise NotImplementedError('Implement this function/method.')
+    Raises
+    ------
+    ValueError
+        if the value of sigma is negative
+    '''
+    if sigma <= 0.01:
+        raise ValueError('Sigma should be positive.')
+    N = max(6*sigma, 3)
+    # If the dimension is even, add 1 to make it odd
+    if N % 2 == 0:
+        N = N + 1
+    # Create one dimension kernel of ascending values
+    x_kernel = np.arange(0, N, step=1, dtype=np.float32)
+    # Apply the gaussian blur equation
+    x_kernel = 1/np.sqrt(2*np.pi*sigma**2) * np.exp((-(x_kernel-math.floor(0.5*N))**2)/(2*sigma**2))
+    # Ensure that the shape of the array is correct
+    x_kernel = x_kernel.reshape((int(len(x_kernel)), 1))
+    # Transpose to create the other dimension
+    y_kernel = np.transpose(x_kernel)
+    y_kernel = y_kernel.reshape((1, int(len(x_kernel))))
+    # Find the sum of the kernel values
+    kernel_sum = (x_kernel*y_kernel).sum()
+    # Do convolutions one dimension at a time
+    conv = convolve(img, y_kernel)
+    conv = convolve(conv, x_kernel)
+    # Return the normalized output
+    return conv/(kernel_sum)
+    raise NotImplementedError('Implement this function/method.')
 
-    def convolve(img, kernel):
-        '''Convenience method around ndimage.convolve.
-        This calls ndimage.convolve with the boundary setting set to 'nearest'.  It
-        also performs some checks on the input image.
-        Parameters
-        ----------
-        img : numpy.ndarray
-            input image
-        kernel : numpy.ndarray
-            filter kernel
-        Returns
-        -------
-        numpy.ndarray
-            filter image
-        Raises
-        ------
-        ValueError
-            if the image is not greyscale
-        TypeError
-            if the image or filter kernel are not a floating point type
-        '''
-        if img.ndim != 2:
-            raise ValueError('Only greyscale images are supported.')
+def convolve(img, kernel):
+    '''Convenience method around ndimage.convolve.
+    This calls ndimage.convolve with the boundary setting set to 'nearest'.  It
+    also performs some checks on the input image.
+    Parameters
+    ----------
+    img : numpy.ndarray
+        input image
+    kernel : numpy.ndarray
+        filter kernel
+    Returns
+    -------
+    numpy.ndarray
+        filter image
+    Raises
+    ------
+    ValueError
+        if the image is not greyscale
+    TypeError
+        if the image or filter kernel are not a floating point type
+    '''
+    if img.ndim != 2:
+        raise ValueError('Only greyscale images are supported.')
 
-        if img.dtype != np.float32 and img.dtype != np.float64:
-            raise TypeError('Image must be floating point.')
+    if img.dtype != np.float32 and img.dtype != np.float64:
+        raise TypeError('Image must be floating point.')
 
-        if kernel.dtype != np.float32 and img.dtype != np.float64:
-            raise TypeError('Filter kernel must be floating point.')
+    if kernel.dtype != np.float32 and img.dtype != np.float64:
+        raise TypeError('Filter kernel must be floating point.')
 
-        return ndimage.convolve(img, kernel, mode='nearest')
+    return ndimage.convolve(img, kernel, mode='nearest')
 
-    
-    def XDoG(img, sigma, row, T):
-        raise NotImplementedError('Implement this function/method.')
-    def LineCleanup():
-        raise NotImplementedError('Implement this function/method.')
+
+def XDoG(img, sigma, p):
+    # If image is RGB
+    if img.ndim == 3:
+        imgGrey = rgb2grey(img)
+    # Else, copy
+    else:
+        imgGrey = np.copy(img)
+    # Apply gaussian blur on the image
+    imgGaus = gaussianBlur(imgGrey, sigma)
+    DoG = imgGaus - gaussianBlur(imgGrey, 1.6*sigma)
+    U = imgGrey + p*DoG
+    T = np.ones((img.shape[0], img.shape[1]))
+    #mask = np.where(U>0.5, 1, 0)
+    #if (U>5):
+    #    T = 1
+    #else:
+    #    T = 0
+    IXDoG = T*U
+    #IXDoG = mask
+
+    return LineCleanup(IXDoG)
+    raise NotImplementedError('Implement this function/method.')
+
+def LineCleanup(img):
+    horzKern = np.array([1, 0, -1])
+    horzKern = horzKern.reshape(1, 3)
+    vertKern = np.transpose(horzKern)
+    Ehorz = convolve(img, horzKern)
+    Evert = convolve(img, vertKern)
+    C = np.logical_and(Ehorz>0, Evert>0)
+    meanKern = (1/9)*np.full((3, 3), 1)
+    B = convolve(img, meanKern)
+    Ifilt = np.copy(img)
+    Ifilt[C] = B[C]
+    return Ifilt
+    raise NotImplementedError('Implement this function/method.')
--- make_cartoon.py
+++ make_cartoon.py
@@ -2,8 +2,9 @@ import numpy as np
 import sys
 import argparse
 from skimage.io import imread, imsave
-from skimage.color import rgb2grey
-from cartoon_effect.cartooner import CartoonEffect
+from skimage.color import rgb2gray
+from cartoon_effect.cartooner import stylization
+from scipy.ndimage import histogram
 '''Project Submission
 Abrar Ahsan, Mahnoor Hussain
 500722182, 500837367
@@ -19,24 +20,28 @@ More inputs will be needed here.
 '''
 
 def cartoonizer(img):
-    converter = CartoonEffect()
     if img.ndim != 3 and img.ndim !=2:
         raise ValueError("Image needs to be RGB or greyscale")
     elif img.ndim == 3:
-        imgGrey = rgb2grey(img)
+        imgGrey = rgb2gray(img)
     else:
         imgGrey = img
-    
-    imgOutput = converter.stylization()
+    #, img, imgGrey, sigmaBlur, row, N)
+    imgStyle = stylization(img, imgGrey, 7, 2.28, 3, 3)
+    #imgOutput = imgStyle*img
+    #imgOutput = (imgOutput/imgOutput.max())*255
 
-    return imgOutput
+    #imgOutput = (imgOutput/imgOutput.max())*255
+
+    return (imgStyle)
 
 if __name__ == '__main__':
-    if (len(sys.argv)<2):
-        raise Exception("Invalid number of arguments")
-    else:
-        print('This need to be implemented!')
-
+    #if (len(sys.argv)<2):
+    #    raise Exception("Invalid number of arguments")
+    #else:
+    #    print('This need to be implemented!')
+    img = imread("E:\Github\Image_Rotoscope\samples\\building.jpg", as_gray = False)
     outputImg = cartoonizer(img)
+    imsave("E:\Github\Image_Rotoscope\Output Folder\output.jpg", (outputImg))
 
     print("New image have been saved in the output folder!")
